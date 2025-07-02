5b005824a58f0737cf4ddadb07a05ea622dcc883
Tarida Grace Wahyuni <2110631170109@student.unsika.ac.id>
2025-07-02 09:40:45 +0700

    C.2: Calibration curve

--- validation.R
+++ validation.R
@@ -8,6 +8,7 @@ head(validasi)
 
 library(ResourceSelection)
 
+
 # 1. HL Test
 
 hl <- hoslem.test(x = validasi$aktual, y = validasi$prediksi, g = 10)
@@ -17,3 +18,17 @@ hl$p.value
 hl$statistic     
 hl$expected      
 hl$observed      
+
+
+# 2. Calibration Curve
+
+p <- ggplot(validasi, aes(x = prediksi, y = aktual)) +
+  geom_point(alpha = 0.3) +
+  geom_smooth(method = "loess") +
+  labs(title = "Calibration Curve",
+       x = "Prediksi Probabilitas",
+       y = "Aktual Default")
+
+print(p)
+
+ggsave("calibration_curve.png")
